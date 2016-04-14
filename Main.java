799b8aa735dfc5f4b2be2352e3f6ee174e3d0731
serkuku <nussersamun@hotmail.com>
2016-04-15 03:37:03 +0700

    #02 create spaceshift
    สร้างตัวยานอวกาศ

--- Main.java
+++ Main.java
@@ -1,18 +1,22 @@
-//package f2.spw;
 
-//import java.awt.BorderLayout;
-
+import java.awt.*;
 import javax.swing.JFrame;
 
-import java.awt.Color;
 
 public class Main {
 	public static void main(String[] args){
 		JFrame frame = new JFrame("Game");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500, 700);
-	
-		frame.getContentPane().setBackground(Color.PINK);
+		frame.setSize(400, 600);
+		frame.getContentPane().setLayout(new BorderLayout());
+		
+
+		SpaceShip s = new SpaceShip(180, 500, 20, 20);
+		GamePanel g = new GamePanel();
+		g.sprites.add(s);
+ 		g.updateGameUI();
+		frame.getContentPane().add(g, BorderLayout.CENTER);
+		frame.getContentPane().setBackground(Color.BLACK);
 		frame.setVisible(true);
 	}
 }
